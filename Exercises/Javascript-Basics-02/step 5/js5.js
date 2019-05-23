c82befa65d3ae35d6f21273dc2b62d744ad1934d
jaafar21 <jaffarsaleh90@gmail.com>
2019-05-23 13:23:57 +0300

    js 02 step5

--- Exercises/Javascript-Basics-02/step 5/js5.js
+++ Exercises/Javascript-Basics-02/step 5/js5.js
@@ -1,4 +1,19 @@
-function changeImage(x){
-    img = document.querySelector("img:nth-child(" + x + ")");
-    img.src = "images/image"+ x + "_2.jpg";
-}
+var x1 =document.querySelector('#image1'); 
+var x2 =document.querySelector('#image2'); 
+var x3 =document.querySelector('#image3'); 
+var x4 =document.querySelector('#image4');
+ var x5 =document.querySelector('#image5');
+  x1.addEventListener("mouseover",x);
+   x2.addEventListener("mouseover",x); 
+  x3.addEventListener("mouseover",x); 
+  x4.addEventListener("mouseover",x); 
+  x5.addEventListener("mouseover",x); 
+  function x (s5){ 
+    if (s5.target.id == "image1")
+   { s5.target.src = "images/image1_2.jpg" 
+   var jpg1 =document.querySelector("image1") }
+    else if(s5.target.id == "image2")
+    { s5.target.src = "images/image2_2.jpg"
+} else if (s5.target.id == "image3"){ s5.target.src ="images/image3_2.jpg" } 
+else if (s5.target.id == "image4"){ s5.target.src = "images/image4_2.jpg" } 
+else if (s5.target.id== "image5") { s5.target.src = "images/image5_2.jpg" } }
--- Exercises/Javascript-Basics-02/step 6/js6.js
+++ Exercises/Javascript-Basics-02/step 6/js6.js
@@ -1,9 +1,28 @@
-function changeImage(x){
-    img = document.querySelector("img:nth-child(" + x + ")");
-    img.src = "images/image"+ x + "_2.jpg";   
-}
 
-function resetImage(x){
-    img = document.querySelector("img:nth-child(" + x + ")");
-    img.src = "images/image"+ x + ".jpg";  
-}
+function mouseOver(x) {
+    if (x == 1) {
+      document.getElementById('image' + x).src = 'images/image2.jpg';
+    } else if (x == 2) {
+      document.getElementById('image' + x).src = 'images/image3.jpg';
+    } else if (x == 3) {
+      document.getElementById('image' + x).src = 'images/image4.jpg';
+    } else if (x == 4) {
+      document.getElementById('image' + x).src = 'images/image5.jpg';
+    } else if (x == 5) {
+      document.getElementById('image' + x).src = 'images/image1.jpg';
+    }
+  }
+  
+  function mouseOut(x) {
+    if (x == 1) {
+      document.getElementById('image' + x).src = 'images/image1.jpg';
+    } else if (x == 2) {
+      document.getElementById('image' + x).src = 'images/image2.jpg';
+    } else if (x == 3) {
+      document.getElementById('image' + x).src = 'images/image3.jpg';
+    } else if (x == 4) {
+      document.getElementById('image' + x).src = 'images/image4.jpg';
+    } else if (x == 5) {
+      document.getElementById('image' + x).src = 'images/image5.jpg';
+    }
+  }
