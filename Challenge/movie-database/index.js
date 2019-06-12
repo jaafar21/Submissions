6301a0b6a34573559b72bb640761c29dd9610861
jaafar21 <jaffarsaleh90@gmail.com>
2019-06-12 13:34:45 +0300

    step 4

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -1,15 +1,24 @@
- const express = require("express");
- const app= express();
- const port = 3000;
- app.listen(port, function () {
+  const express = require("express");
+  const app= express();
+  const port = 3000;
+  app.listen(port, function () {
     console.log("Server is running on "+ port +" port");
   });
+//////*step3*/
   app.get('/', (req, res) => { 
       res.send('<h1>Ok</h1>'); }); 
       app.get('/test', (req, res) => {
     res.send({status:200, message:"ok"}); });
-app.get('/time', (req, res) => { 
+
+  app.get('/time', (req, res) => { 
     var time = new Date (); 
     res.send({status:200, message: time.getHours() + ":"+ time.getMinutes() + ":" + time.getSeconds()}); });
-app.get('/hello/:id?', (req, res) =>{ 
-    if(req.params.id !== undefined){ res.send ({ status:200, message:"Hello," + req.params.id })} else { res.send ({ status:200, message:"Hello," })} });
+   /*STEP 4*////////////
+  app.get('/hello/:id?', (req, res) =>{ 
+    if(req.params.id !== undefined){ res.send ({ status:200, message:"Hello," + req.params.id })} 
+    else { res.send ({ status:200, message:"Hello," })} });
+ app.get('/search', (req, res) =>{ 
+    if(req.query.s !== undefined && req.query.s !== ""){
+    res.send({status:200, message:"ok", data:req.query.s})
+      } else {
+     res.send({status:500, error:true, message:"you have to provide a search"}) } } );
