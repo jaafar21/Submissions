1807251bb9a43e8cfbd1d2adf474342fe11048c5
jaafar21 <jaffarsaleh90@gmail.com>
2019-06-12 12:38:22 +0300

    step 3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -1,10 +1,15 @@
-const express = require("express");
-const app= express();
-const port = 3000;
-app.get('/', (req, res) => { 
-res.send('<h1>Ok</h1>');
-});
-
-app.listen(port, function () {
+ const express = require("express");
+ const app= express();
+ const port = 3000;
+ app.listen(port, function () {
     console.log("Server is running on "+ port +" port");
   });
+  app.get('/', (req, res) => { 
+      res.send('<h1>Ok</h1>'); }); 
+      app.get('/test', (req, res) => {
+    res.send({status:200, message:"ok"}); });
+app.get('/time', (req, res) => { 
+    var time = new Date (); 
+    res.send({status:200, message: time.getHours() + ":"+ time.getMinutes() + ":" + time.getSeconds()}); });
+app.get('/hello/:id?', (req, res) =>{ 
+    if(req.params.id !== undefined){ res.send ({ status:200, message:"Hello," + req.params.id })} else { res.send ({ status:200, message:"Hello," })} });
