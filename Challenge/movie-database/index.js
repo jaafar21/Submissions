517e7787f45a0e44fbb43d76019dc9b68b97fd07
jaafar21 <jaffarsaleh90@gmail.com>
2019-06-12 14:25:22 +0300

    step 5

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -4,9 +4,16 @@
   app.listen(port, function () {
     console.log("Server is running on "+ port +" port");
   });
+  const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
 //////*step3*/
   app.get('/', (req, res) => { 
       res.send('<h1>Ok</h1>'); }); 
+
       app.get('/test', (req, res) => {
     res.send({status:200, message:"ok"}); });
 
@@ -22,3 +29,13 @@
     res.send({status:200, message:"ok", data:req.query.s})
       } else {
      res.send({status:500, error:true, message:"you have to provide a search"}) } } );
+            //////////*step 5*/
+            app.get('/movies/read', (req, res) => {
+                res.send({status:200, message:"movies"}); });
+       
+                app.get('/movies/edit', (req, res) => {
+                    res.send({status:200, message:""}); });
+                    app.get('/movies/delete', (req, res) => {
+                        res.send({status:200, message:""}); });
+                        app.get('/movies/create', (req, res) => {
+                            res.send({status:200, message:""}); });
